6ca7c4d05b1f73ccaa99f518d6372ab470daee93
Bemgba-Apeagee <140463947+Bemgba-Apeagee@users.noreply.github.com>
2025-10-21 18:28:07 +0100

    description of table updated

--- task_4.sql
+++ task_4.sql
@@ -1,4 +1,16 @@
-USE alx_book_store;
-
--- Print the full description of the table 'books'
-SHOW CREATE TABLE Books;
+-- Print full description of the table 'Books'
+SELECT
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_DEFAULT,
+    COLUMN_KEY,
+    EXTRA,
+    ORDINAL_POSITION
+FROM
+    INFORMATION_SCHEMA.COLUMNS
+WHERE
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'Books'
+ORDER BY
+    ORDINAL_POSITION;
