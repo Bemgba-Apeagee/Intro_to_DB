ad406ea347be16fe0e89e28aaaaf63d7f08bef79
Bemgba-Apeagee <140463947+Bemgba-Apeagee@users.noreply.github.com>
2025-10-21 18:02:39 +0100

    except error connector updated for mysqlserver

--- MySQLServer.py
+++ MySQLServer.py
@@ -21,8 +21,8 @@ def create_database():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
-        print(f"mysql.connector.Error: {e}")
+    except mysql.connector.Error as e:
+        print(f"Error while connecting to MySQL: {e}")
 
     finally:
         # Closing your database
